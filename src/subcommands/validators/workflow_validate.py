5371479df2a66dab0258473b4ab017abb0987027
Alexis Lucattini <alexis.lucattini@umccr.org>
2021-05-13 16:23:11 +1000

    Added in validation commands to confirm cwl type
    conforms before changing workflow definitions on ICA.

---
+++ src/subcommands/validators/workflow_validate.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+
+"""
+Validate a cwl tool
+"""
+
+from subcommands.validators.validate import Validate
+from utils.logging import get_logger
+from docopt import docopt
+from argparse import ArgumentError
+from utils.repo import get_workflows_dir
+from classes.cwl_workflow import CWLWorkflow
+from pathlib import Path
+from utils.errors import CheckArgumentError
+
+logger = get_logger()
+
+
+class WorkflowValidate(Validate):
+    """Usage:
+    cwl-ica workflow-validate help
+    cwl-ica workflow-validate (--workflow-path="<path_to_cwl_workflow>")
+
+Description:
+    Validate a CWL File of CommandLineTool. This must be done prior to registering the workflow with the "cwl-ica workflow-init" command.
+    The CWL workflow must be in the workflows/ directory under CWL_ICA_REPO_PATH
+
+Options:
+    --workflow-path=<workflow_path>      Required, the path to the cwl workflow
+
+Example
+    cwl-ica workflow-validate --workflow-path workflows/dragen-germline/3.7.5/dragen-germline__3.7.5.cwl
+    """
+
+    def __init__(self, command_argv):
+        """
+        Check arguments - check cwl file path exists
+        Import cwl object
+        :param command_argv:
+        """
+        # Collect args from doc strings
+        super().__init__(command_argv)
+
+        # Check help
+        self.check_length(command_argv)
+
+        # Check if help has been called
+        if self.args["help"]:
+            self._help()
+
+        # Confirm 'required' arguments are present and valid
+        try:
+            self.check_args()
+        except ArgumentError:
+            self._help(fail=True)
+
+    def __call__(self):
+        super(WorkflowValidate, self).__call__()
+
+    def check_args(self):
+        """
+        Check --tool-path exists
+        :return:
+        """
+
+        # Check defined and assign properties
+        workflow_path_arg = self.args.get("--workflow-path", None)
+        if workflow_path_arg is None:
+            logger.error("--workflow-path not defined")
+            raise CheckArgumentError
+        self.cwl_file_path = Path(workflow_path_arg)
+        # Checks cwl_file_path
+        self.check_file()
+
+        # Make sure file path exists
+        self.name, self.version = self.split_name_version(items_dir=self.get_top_dir())
+
+        # Check name
+        self.check_shlex_arg("--workflow-path", self.name)
+
+        # Check version
+        self.check_shlex_version_arg("--workflow-path", self.version)
+
+        # Set cwl obj
+        self.cwl_obj = self.import_cwl_obj()
+
+    def import_cwl_obj(self):
+        """
+        Create a cwl object
+        :return:
+        """
+
+        return CWLWorkflow(
+            cwl_file_path=self.cwl_file_path,
+            name=self.name,
+            version=self.version,
+            create=False,
+            user_obj=None
+        )
+
+    @staticmethod
+    def get_top_dir(create_dir=False):
+        """
+        Returns <CWL_ICA_REPO_PATH>/tools
+        :return:
+        """
+        return get_workflows_dir(create_dir=create_dir)
