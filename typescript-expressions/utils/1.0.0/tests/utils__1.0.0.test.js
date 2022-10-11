9f9f66178b77330d630c8a35937bf409a3742012
Alexis Lucattini <Alexis.Lucattini@umccr.org>
2022-10-11 14:25:24 +1100

    Updated utils to add function get_attribute_from_optional_input

--- typescript-expressions/utils/1.0.0/tests/utils__1.0.0.test.js
+++ typescript-expressions/utils/1.0.0/tests/utils__1.0.0.test.js
@@ -5,11 +5,16 @@
 // Imports
 Object.defineProperty(exports, "__esModule", { value: true });
 var utils__1_0_0_1 = require("../utils__1.0.0");
+var cwl_ts_auto_1 = require("cwl-ts-auto");
 var FALSE = false;
 var TRUE = true;
 var NULL = null;
 var UNDEFINED = undefined;
 var NOT_NULL = "This string is not null";
+var NOT_NULL_FILE = {
+    class_: cwl_ts_auto_1.File_class.FILE,
+    location: "/path/to/bar"
+};
 // Test get value as str
 describe('Test get_bool_value_as_str', function () {
     test('We expect get_bool_value_as_str(NULL) to be "false"', function () {
@@ -20,6 +25,15 @@ describe('Test get_bool_value_as_str', function () {
     });
     test('We expect get_bool_value_as_str(TRUE) to be "true"', function () {
         expect((0, utils__1_0_0_1.get_bool_value_as_str)(TRUE)).toEqual("true");
+    });
+});
+// get_attribute_from_optional_input test
+describe('Test get_attribute_from_optional_input utils', function () {
+    test('We expect get_attribute_from_optional_input(UNDEFINED) to be null', function () {
+        expect((0, utils__1_0_0_1.get_attribute_from_optional_input)(UNDEFINED, "location")).toEqual(NULL);
+    });
+    test('We expect get_attribute_from_optional_input(UNDEFINED) to be false', function () {
+        expect((0, utils__1_0_0_1.get_attribute_from_optional_input)(NOT_NULL_FILE, "location")).toEqual(NOT_NULL_FILE["location"]);
     });
 });
 // is_not_null test
