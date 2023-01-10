613d15d3774f215eb28d1b39c2f5e9ca5a14a733
Alexis Lucattini <Alexis.Lucattini@umccr.org>
2023-01-10 12:09:37 +1100

    Use create-release github actions script to handle subcommand

---
+++ .github/scripts/create_workflow_release_asset.sh
@@ -0,0 +1,98 @@
+#!/usr/bin/env bash
+
+# Create the workflow release asset
+
+# Set Globals
+export CWL_ICA_REPO_PATH="$PWD"
+
+help_message="Usage: create_workflow_release_asset.sh
+Wrapper script around cwl-ica github-actions-build-workflow-release-asset subcommand
+Optional parameters:
+         --is-pre-release: Add parameter --is-pre-release to subcommand
+         --workflow-path: Add workflow path parameter to subcommand
+"
+
+
+# FUNCTIONS
+echo_stderr(){
+  : '
+  Write out to stderr
+  '
+  echo "${@}" 1>&2
+}
+
+print_help() {
+  echo_stderr "${help_message}"
+}
+
+# Check for the following env vars exist
+
+# Check env vars
+if [[ ! -v GITHUB_SERVER_URL ]]; then
+  echo "Error - expected env var GITHUB_SERVER_URL" 1>&2
+  exit 1
+fi
+
+if [[ ! -v GITHUB_REPOSITORY ]]; then
+  echo "Error - expected env var GITHUB_REPOSITORY" 1>&2
+  exit 1
+fi
+
+if [[ ! -v GITHUB_TOKEN ]]; then
+  echo "Error - expected env var GITHUB_TOKEN" 1>&2
+  exit 1
+fi
+
+if [[ ! -v GITHUB_TAG ]]; then
+  echo "Error - expected env var GITHUB_TAG" 1>&2
+  exit 1
+fi
+
+
+# Get args
+
+# is pre-release
+
+# Check workflow path
+
+# Run create release asset command
+is_prerelease="false"
+workflow_path=""
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --is-prerelease)
+      is_prerelease="$2"
+      shift 1
+      ;;
+    --workflow-path)
+      workflow_path="$2"
+      shift 1
+      ;;
+    -h|--help)
+      print_help
+      exit 0
+      ;;
+    *)
+      print_help
+      exit 1
+      ;;
+  esac
+  shift 1
+done
+
+# Check workflow path is readable
+if [[ ! -r "${workflow_path}" ]]; then
+  echo_stderr "Could not read file ${workflow_path}"
+fi
+
+# Create parameter list
+arg_array=( "--workflow-path" "${workflow_path}" )
+
+# Extend parameter list with '--draft-release' if this IS a pre-release
+if [[ "${is_prerelease}" == "true" ]]; then
+  arg_array+=( "--draft-release" )
+fi
+
+# Launch cwl-ica github-actions-build-workflow-release-asset
+eval cwl-ica github-actions-build-workflow-release-asset '"${arg_array[@]}"'
