2b7dba4c9a08655b27b70135ea4fa8beb63fead2
Alexis Lucattini <Alexis.Lucattini@umccr.org>
2025-08-31 14:18:25 +1000

    Update dragen nirvana annotation options (as per copilot suggestion)

--- schemas/dragen-nirvana-annotation-options/4.4.4/dragen-nirvana-annotation-options__4.4.4.ts
+++ schemas/dragen-nirvana-annotation-options/4.4.4/dragen-nirvana-annotation-options__4.4.4.ts
@@ -1,21 +1,24 @@
-import {FileProperties as IFile} from "cwl-ts-auto"
+import { FileProperties as IFile } from "cwl-ts-auto"
 
 export interface DragenNirvanaAnnotationOptions {
-    /*
-    enable variant annotation:
+	/*
+	enable variant annotation: 
+	Enable Nirvana variant annotation on the output vcf/gvcf files.
+	The default is false.
+	*/
+	enable_variant_annotation?: boolean
 
-    */
-    enable_variant_annotation?: boolean
+	/*
+	variant annotation assembly: 
+	The reference genome assembly used for Nirvana variant annotation.
+	One of 'GRCh37' / 'GRCh38'
+	*/
+	variant_annotation_assembly?: string
 
-    /*
-    variant annotation assembly:
-
-    */
-    variant_annotation_assembly?: IFile
-
-    /*
-    variant annotation data:
-
-    */
-    variant_annotation_data?: string
+	/*
+	variant annotation data: 
+	Tarball containing Nirvana data. 
+	If not provided, but enable_variant_annotation is true, the data will be downloaded from Illumina's servers
+	*/
+	variant_annotation_data?: IFile
 }
